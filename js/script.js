cdc757ce5794c9cbb8ed1ddcc29541c584b02dd8
Oscar Meza <omeza@globalsoft.email>
2025-07-18 16:19:24 -0600

    Navegación y menú

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function toggleNav() {
+    const nav = document.querySelector('.nav');
+    const menu = document.querySelector('.menu');
+    nav.classList.toggle('active');
+    menu.classList.toggle('active');
+}
